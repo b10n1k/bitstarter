fa9ee864fafb14f66857c7b60ee1e6b1ee2abefd
b10n1k <jbonatakis@gmail.com>
2013-07-07 22:07:41 +0000

    fix buffer to work with index.html

--- web.js
+++ web.js
@@ -5,8 +5,9 @@ var app = express.createServer(express.logger());
 var buffer = new Buffer(24);
 
 app.get('/', function(request, response) {
-  txt=buffer.toString(fs.readFileSync("index.html"));
-  response.send(txt);
+
+  buffer.write(fs.readFileSync("index.html"));
+  response.send(buffer.toString("utf8");
 });
 
 var port = process.env.PORT || 5000;
