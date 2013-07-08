9a0f9413c104d0ac7a6e691e0725a47b930246b4
b10n1k <jbonatakis@gmail.com>
2013-07-08 21:31:19 +0000

    fix_3 Make it synchronous

--- web.js
+++ web.js
@@ -5,12 +5,12 @@ var app = express.createServer(express.logger());
 var buffer = new Buffer(256);
 
 buffer = fs.readFileSync("index.html");
-//buffer.toString("utf8");
+//txt = buffer.toString("utf8");
 
 app.get('/', function(request, response) {
 
 //  buffer=fs.readFileSync("index.html");
-  response.send(buffer.toString();
+  response.send(buffer.toString());
 //});
 
 var port = process.env.PORT || 5000;
